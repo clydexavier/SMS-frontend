022a60d6eec995567fe01e439d3162d03530c6b2
clydexavier <salarclydexavier@gmail.com>
2025-03-21 18:31:11 +0800

    Update: Category in EventModal now a listbox

--- src/components/admin/EventModal.jsx
+++ src/components/admin/EventModal.jsx
@@ -113,7 +113,7 @@ export default function EventModal({
                   />
                 </div>
     
-                {/* Type */}
+                {/* Tournament Type */}
                 <div className="col-span-2">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Tournament Type
@@ -128,7 +128,24 @@ export default function EventModal({
                     required
                   />
                 </div>
-    
+                {/* Category */}
+                <div className="col-span-2">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Category
+                  </label>
+                  <select
+                    name="category"
+                    value={formData.category}
+                    onChange={handleChange}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    required
+                  >
+                    <option value="" disabled>Select event category</option>
+                    <option value="Men">Men</option>
+                    <option value="Women">Women</option>
+                    <option value="Mixed">Mixed</option>
+                  </select>
+                </div>
                 {/* Gold */}
                 <div className="col-span-2">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">

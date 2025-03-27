5226157c6d6a1cff53442b174f9a383a1cc0f3bb
clydexavier <salarclydexavier@gmail.com>
2025-03-27 18:21:57 +0800

    [FEAT] Added AddTeam modal

--- src/components/admin/TeamModal.jsx
+++ src/components/admin/TeamModal.jsx
@@ -14,6 +14,21 @@ export default function TeamModal({
     previewLogo: "", // Stores preview URL for the logo
   });
 
+  const [dragText, changeDragText] = useState(
+    <>
+      <span className="font-semibold">Click to upload logo</span> or drag and
+      drop
+    </>
+  );
+
+  const [dragLabel, setDragLabel] = useState(
+    <p className="text-xs text-gray-500 dark:text-gray-400">
+      SVG, PNG, JPG or GIF (MAX. 800x400px)
+    </p>
+  );
+
+  const [uploaded, setUploaded] = useState(false);
+
   useEffect(() => {
     if (existingTeam) {
       setFormData({
@@ -24,6 +39,18 @@ export default function TeamModal({
     } else {
       setFormData({ name: "", logo: null, previewLogo: "" });
     }
+
+    changeDragText(
+      <>
+        <span className="font-semibold">Click to upload logo</span> or drag and
+        drop
+      </>
+    );
+    setDragLabel(
+      <p className="text-xs text-gray-500 dark:text-gray-400">
+        SVG, PNG, JPG or GIF (MAX. 800x400px)
+      </p>
+    );
   }, [existingTeam]);
 
   const handleChange = (e) => {
@@ -31,15 +58,52 @@ export default function TeamModal({
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const uploadFile = (files) => {
+    if (!files) return;
+
+    const file = files[0];
+
+    changeDragText(<span className="font-semibold">{file.name}</span>);
+    setDragLabel(undefined);
+
+    setFormData((prevData) => ({
+      ...prevData,
+      logo: file,
+      previewLogo: URL.createObjectURL(file),
+    }));
+
+    setUploaded(true);
+  };
+
   const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setFormData((prevData) => ({
-        ...prevData,
-        logo: file,
-        previewLogo: URL.createObjectURL(file),
-      }));
+    e.preventDefault();
+    uploadFile(e.target.files);
+  };
+
+  const fileUploadDragOver = (e) => {
+    e.preventDefault();
+    changeDragText(
+      <span className="font-semibold">Release to upload file.</span>
+    );
+  };
+
+  const fileUploadDragLeave = (e) => {
+    e.preventDefault();
+
+    if (uploaded) {
+      return;
     }
+    changeDragText(
+      <>
+        <span className="font-semibold">Click to upload logo</span> or drag and
+        drop
+      </>
+    );
+  };
+
+  const fileUploadDrop = (e) => {
+    e.preventDefault();
+    uploadFile(e.dataTransfer.files);
   };
 
   const handleSubmit = (e) => {
@@ -103,20 +167,46 @@ export default function TeamModal({
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Team Logo
                   </label>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    required={!existingTeam}
-                  />
-                  {formData.previewLogo && (
-                    <img
-                      src={formData.previewLogo}
-                      alt="Team Logo"
-                      className="mt-2 w-24 h-24 object-cover rounded-lg"
-                    />
-                  )}
+                  <div className="flex items-center justify-center w-full">
+                    <label
+                      htmlFor="dropzone-file"
+                      className="flex flex-col items-center justify-center w-full h-[50%] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                      onChange={handleFileChange}
+                      onDragOver={fileUploadDragOver}
+                      onDragLeave={fileUploadDragLeave}
+                      onDrop={fileUploadDrop}
+                    >
+                      <div
+                        className="flex flex-col items-center justify-center pt-5 pb-6"
+                        style={{ pointerEvents: "none" }}
+                      >
+                        <svg
+                          className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 20 16"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                          />
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          {dragText}
+                        </p>
+                        {dragLabel}
+                      </div>
+                      <input
+                        id="dropzone-file"
+                        type="file"
+                        className="hidden"
+                      />
+                    </label>
+                  </div>
                 </div>
               </div>
 

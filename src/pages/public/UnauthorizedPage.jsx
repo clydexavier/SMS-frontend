ccf9a17dcf67748c910bc88d1a7fdd5d93c1c2d9
clyde <irishgrace@Irishs-MacBook-Pro.local>
2025-04-15 00:07:24 +0800

    working on mac

--- src/pages/public/UnauthorizedPage.jsx
+++ src/pages/public/UnauthorizedPage.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import axiosClient from '../../axiosClient'
+import { useStateContext } from '../../context/ContextProvider'
 
 export default function UnauthorizedPage() {
+    const { setUser, setToken, setRole,user } = useStateContext();
+  
+    const onLogout = async (ev) => {
+    ev.preventDefault();
+    try {
+      localStorage.removeItem("ACCESS_TOKEN");
+      SetUser(null);
+      setToken(null);
+      setRole(null);
+      await axiosClient.get('/logout');
+      setUser(null);
+      setToken(null);
+      setRole(null);
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
   return (
     <div>
         <h1>
             Unauthorized Page
         </h1>
+        <button onClick={onLogout}>Logout</button>
     </div>
   )
 }

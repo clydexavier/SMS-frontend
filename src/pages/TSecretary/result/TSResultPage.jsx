ef09a7ffd2875057c8cb2cec521ecfab35e6aa16
clydexavier <salarclydexavier@gmail.com>
2025-05-29 04:05:23 +0800

    Added match number to games page t secretary

--- src/pages/TSecretary/result/TSResultPage.jsx
+++ src/pages/TSecretary/result/TSResultPage.jsx
@@ -117,7 +117,7 @@ const TSResultPage = () => {
     if (eventStatus !== "pending" || loading) return null;
     
     return (
-      <div className="bg-yellow-50 border-yellow-200 flex flex-col w-full border rounded-xl shadow-md p-8 text-center">
+      <div className="flex-1 bg-yellow-50 border-yellow-200 flex flex-col w-full border rounded-xl shadow-md p-8 text-center">
         <svg className="w-12 h-12 mx-auto mb-2 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>
         </svg>
